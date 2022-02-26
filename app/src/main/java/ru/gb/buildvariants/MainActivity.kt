3edda6c355327ab85dfe27a8a918c8a64c21fef6
vikzak <victor@azbukait.ru>
2022-02-26 21:41:15 +0300

    Initial commit

---
+++ app/src/main/java/ru/gb/buildvariants/MainActivity.kt
@@ -0,0 +1,17 @@
+package ru.gb.buildvariants
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.gb.buildvariants.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.textView.text = getString(R.string.hello)
+    }
+}
